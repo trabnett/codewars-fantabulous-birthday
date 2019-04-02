8aaea3e4a2efe3973aaa6d38b7d94677c020cd6e
trabnett <timrabnett@gmail.com>
2019-04-02 10:17:49 -0400

    refactored with arrow functions and ternary opperators

--- birthday.js
+++ birthday.js
@@ -1,24 +1,16 @@
-function fantabulousBirthday(S){
+fantabulousBirthday = (S) => {
     corner = nextHighestSquareRoot(S)
     idx = (corner ** 2) - S + 1
-    if (corner % 2 === 0){
-        if (idx < corner) {
-            return [corner, idx]
-        } else {
-            return [(corner * 2) - idx, corner]
-        }
-    } else {
-        if (idx <= corner) {
-            return [idx, corner]
-        } else {
-            return [corner, (corner * 2 - idx)]
-        }
-
-    }
+    return corner % 2 === 0 ? evenSquareResult(corner, idx) : oddSquareResult(corner, idx)
+}
+evenSquareResult = (corner, idx) => {
+    return idx < corner ? [corner, idx] : [(corner * 2) - idx, corner]
+}
+oddSquareResult = (corner, idx) => {
+    return idx <= corner ? [idx, corner] : [corner, (corner * 2 - idx)]
 }
 
 nextHighestSquareRoot = (n) => {
     return (Math.sqrt(n) % 2 === 0) ? Math.sqrt(n) : Math.ceil(Math.sqrt(n))
 }
 
-console.log(fantabulousBirthday(1))
